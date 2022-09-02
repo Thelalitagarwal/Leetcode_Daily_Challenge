dcc1baccba4750e879b842b42a7153fd8ba8b3ea
Thelalitagarwal <77659094+Thelalitagarwal@users.noreply.github.com>
2022-09-02 09:18:07 +0530

    2_September_2022

---
+++ Average of Levels in Binary Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            long temp=0;
+            int size= q.size();
+            for(int i=0;i<size;i++){
+                TreeNode * node = q.front();
+                q.pop();
+                if(node->left!=NULL) q.push(node->left);
+                if(node->right!=NULL) q.push(node->right);
+                temp+=node->val;
+            }
+            ans.push_back((double)temp/size);
+        }
+        return ans;
+    }
+};
