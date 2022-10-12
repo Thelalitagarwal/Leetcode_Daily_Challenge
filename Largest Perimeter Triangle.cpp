fb1957175fdd2043102f0ac19f0778708823499b
Lalit Agarwal <lalitagarwal8278@gmail.com>
2022-10-12 08:35:48 +0530

    12_October_2022

---
+++ Largest Perimeter Triangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=nums.size()-1;i>=2;i--)
+            if(nums[i]<nums[i-1]+nums[i-2])
+                return nums[i]+nums[i-1]+nums[i-2];
+        return 0;
+    }
+};
