b65fadbb9af09a065f6d134df885acf25e372c86
Lalit Agarwal <lalitagarwal8278@gmail.com>
2022-11-20 09:05:02 +0530

    20_November_2022

---
+++ Basic Calculator.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack <long long int> nums, ops;
+        long long int num = 0;
+        long long int rst = 0;
+        long long int sign = 1;
+        for (char c : s) { 
+            if (isdigit(c)) {
+                num = num * 10 + c - '0';
+            }
+            else {
+                rst += sign * num;
+                num = 0;
+                if (c == '+') 
+                    sign = 1;
+                if (c == '-') 
+                    sign = -1;
+                if (c == '(') {
+                    nums.push(rst);
+                    ops.push(sign);
+                    rst = 0;
+                    sign = 1;
+                }
+                if (c == ')' && ops.size()) {
+                    rst = ops.top() * rst + nums.top();
+                    ops.pop(); nums.pop();
+                }
+            }
+        }
+        rst += sign * num;
+        return rst;
+    }
+};
