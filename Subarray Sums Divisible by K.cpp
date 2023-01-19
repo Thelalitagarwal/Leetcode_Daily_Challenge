7b8b1787a38aacce4dd4e14d66e28d7e578da98d
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-01-19 10:46:29 +0530

    updated 19_January_2023

--- Subarray Sums Divisible by K.cpp
+++ Subarray Sums Divisible by K.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int subarraysDivByK(vector<int>& nums, int k) {
-        vector<int> output(k);
-        int prefixMod = 0;
-        int res = 0;
-        output[0] = 1;
-        for(int i = 0; i < nums.size(); i++){
-            prefixMod = (prefixMod + nums[i] % k + k) % k;
-            res = res + output[prefixMod];
-            output[prefixMod]++;
+    int subarraysDivByK(vector<int>& A, int K) {
+        unordered_map<int, int> mp;
+        int sum = 0, ans = 0;
+        mp[0] = 1;
+        for(int n: A){
+            sum = ((sum + n) % K + K) % K;
+            mp[sum] ++;
+            if(mp[sum] > 1){
+                ans += mp[sum] - 1;
+            }
         }
-        return res;
+        return ans;
     }
 };
