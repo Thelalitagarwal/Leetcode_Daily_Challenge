771d14b832459e8ee0b3e716ebfe1fc4164095e9
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-02-05 10:19:18 +0530

    05_February_2023

---
+++ Zigzag Conversion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<vector<char>> a(numRows, vector<char>(s.size()));
+        int row = 0, dir = true;
+        for(int i = 0; i < s.size(); i++){
+            a[row%numRows][i] = s[i];
+            if(dir)
+                ++row;
+            else
+                --row;
+            
+            if(row == numRows-1)
+                dir = false;
+            else if(row == 0)
+                dir = true;
+                
+        }
+        
+        string r;
+        for(auto x : a)
+            for(auto y : x)
+                if(isalpha(y) || y == '.' || y == ',')
+                    r+=y;
+        return r;
+    }
+};
