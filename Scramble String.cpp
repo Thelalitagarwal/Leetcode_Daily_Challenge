97981a80c0a1747bffc809e22db4a388ead6f89a
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-03-30 10:45:21 +0530

    30-march-2023

--- Scramble String.cpp
+++ Scramble String.cpp
@@ -1,19 +1,4 @@
 class Solution {
-  public:
-    int minimumInteger(int N, vector<int> &A) {
-        long long int sum=0;
-        for(auto it:A){
-             sum+=it;
-        }
-        int ans=INT_MAX;
-        for(int i=0;i<N;i++){
-            if((long long)N*A[i]>=sum){
-               ans=min(ans,A[i]);  
-            }
-        }
-        return ans;
-    }
-};class Solution {
         bool DP_helper(unordered_map<string, bool> &isScramblePair, string s1, string s2)
         {
             int len = s1.size();
