6322edb7e40f810e1b5d72576dfcb4696614edbe
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-08-07 09:58:05 +0530

    07_August_2023

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -1,3 +1,14 @@
+/*
+Link to the "Search a 2D Matrix" Problem ==>> https://leetcode.com/problems/search-a-2d-matrix/ 
+   
+Link to the complete Explaination Video ==>> https://youtu.be/mVv4IpBHpMo
+
+*/
+
+// 2 Approaches are attached in this please check wisely
+
+// 1st Method
+
 class Solution {
 public:    
     bool searchMatrix(vector<vector<int>>& mat, int target) {
@@ -14,3 +25,35 @@ public:
     }
 };
 
+
+// 2 Method
+
+/*
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int low = 0;
+        int high = (row*col)-1;
+
+        while(low<=high){
+
+            int mid = high+(low-high)/2;
+
+            if(matrix[mid/col][mid%col]==target)
+                return true;
+
+            else if(matrix[mid/col][mid%col]<target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+
+        return false;
+    }
+};
+
+*/
