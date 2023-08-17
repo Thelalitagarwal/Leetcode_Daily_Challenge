f97e9e8d47418a02f01f80ddfb045a1aee6013f9
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-08-17 10:10:58 +0530

    17_August_2023

---
+++ 01 Matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+
+        queue<pair<int, int>> q;
+
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (mat[r][c] == 0) 
+                    q.push({r, c});
+                else mat[r][c] = -1;
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); 
+            q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + DIR[i], nc = c + DIR[i+1];
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) 
+                    continue;
+                
+                mat[nr][nc] = mat[r][c] + 1;
+                q.push({nr, nc});
+            }
+        }
+        return mat;
+    }
+};
