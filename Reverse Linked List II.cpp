628759539ab17e9ac2b7fb5b1477c90b15bcd717
Lalit Agarwal <lalitagarwal8278@gmail.com>
2023-09-07 09:21:33 +0530

    07_September_2023

---
+++ Reverse Linked List II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head) 
+            return nullptr;
+
+        ListNode* dummy = new ListNode(0,head);
+        head = dummy;
+        ListNode* curr = head->next;
+        ListNode* prev = dummy;
+
+        for(int i = 1; i < left; i++) {
+            prev = prev->next;
+        }
+
+        ListNode* left_prev = prev->next;
+
+        for(int i=0; i< right - left;i++){
+
+            ListNode* temp = prev->next;
+            prev->next = left_prev->next;
+
+            left_prev->next = left_prev->next->next;
+            
+            prev->next->next = temp; 
+        }
+        return dummy->next;
+    }
+};
